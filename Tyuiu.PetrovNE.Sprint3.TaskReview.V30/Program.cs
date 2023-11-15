abc7d0bc9bb9b15f6835b4ca63b49bbe6a4da9dd
PetrovNikita a.k Shugizawa <silestrange@gmail.com>
2023-11-15 10:44:35 +0500

    Add project files.

---
+++ Tyuiu.PetrovNE.Sprint3.TaskReview.V30/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PetrovNE.Sprint3.TaskReview.V30.Lib;
+
+namespace Tyuiu.PetrovNE.Sprint3.TaskReview.V30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил: Петров Н. Е.  | АСОиУБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: SprintReview                                                      *");
+            Console.WriteLine("* Задание #SprintReview                                                   *");
+            Console.WriteLine("* Вариант #30                                                             *");
+            Console.WriteLine("* Выполнил: Петров Никита Евгеньевич | АСОиУБ-23-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая выводит таблицу значений функции:           *");
+            Console.WriteLine("* F(x) = ((5x + 2,5)/(sin(x) + 2)) + 2x + 2                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+            double[] valueArray;
+            valueArray = new double[len];
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("+----------+----------+");
+            Console.WriteLine("|    X     |   f(x)   |");
+            Console.WriteLine("+----------+----------+");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("|{0,5:d}     |{1, 6:f2}    |", startValue, valueArray[i]);
+                Console.WriteLine("+----------+----------+");
+                startValue++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
